1c0f4f3a3d1eefafd567a7cd2deba9ec1cd05c01
sebastienmoulin <sebastien.m.web@gmail.com>
2022-10-26 17:27:59 +0200

    J1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,25 @@
-import "./App.css";
+import { useEffect } from "react";
+import ContactForm from "./components/ContactForm";
 
 function App() {
+  useEffect(() => {
+    if (document) {
+      const stylesheet = document.createElement("link");
+      stylesheet.rel = "stylesheet";
+      stylesheet.href =
+        "https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css";
+
+      document.head.appendChild(stylesheet);
+    }
+  }, []);
+
   return (
-    <div className="">
-      <p>coucou</p>
+    <div className="App">
+      <header className="App-header">
+        <div className="py-6">
+          <ContactForm />
+        </div>
+      </header>
     </div>
   );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      backgreen: "#99C1B9",
+      bargreen: "#15616D",
+      backcolor: "#FFECD1",
+    },
     extend: {},
   },
   plugins: [],
