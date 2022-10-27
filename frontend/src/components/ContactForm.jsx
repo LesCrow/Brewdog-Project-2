c842f6bbd60cc6fbbf2cace0c6488b0d2bf863ce
sebastienmoulin <sebastien.m.web@gmail.com>
2022-10-27 15:17:14 +0200

    Jour1

--- frontend/src/components/ContactForm.jsx
+++ frontend/src/components/ContactForm.jsx
@@ -13,8 +13,8 @@ const ContactForm = () => {
   if (submitted) {
     return (
       <>
-        <div className="text-2xl">Thank you!</div>
-        <div className="text-md">We'll be in touch soon.</div>
+        <div className="text-2xl">Merci!</div>
+        <div className="text-md">Nous vous contacterons bientôt.</div>
       </>
     );
   }
@@ -25,23 +25,27 @@ const ContactForm = () => {
       items-center justify-center bg-backcolor"
     >
       <div className="mb-3 pt-0">
-        <h1 className="text-bargreen text-center text-4xl  text-stroke">
-          CONTACT
+        <h1 className="text-2xl font-bold">
+          <a />
         </h1>
+        <h2 className="text-bargreen text-center text-4xl  text-stroke">
+          CONTACT
+        </h2>
       </div>
       <form
-        className="bg-backgreen border 3px shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        className="bg-backgreen border 3px border-black	 shadow-md rounded px-8 pt-6 pb-8 mb-4"
         action={FORM_ENDPOINT}
         onSubmit={handleSubmit}
         method="POST"
         target="_blank"
       >
-        <div className="mb-3 pt-0">
+        <p>LAISSEZ NOUS UN MESSAGE</p>
+        <div className="mb-3 pt-0 my-8">
           <input
             type="text"
-            placeholder="Your name"
+            placeholder="Nom"
             name="name"
-            className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+            className="border 3px border-black	px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm  shadow outline-none focus:outline-none focus:ring w-full"
             required
           />
         </div>
@@ -50,27 +54,32 @@ const ContactForm = () => {
             type="email"
             placeholder="Email"
             name="email"
-            className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+            className="border 3px border-black px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm  shadow outline-none focus:outline-none focus:ring w-full"
             required
           />
         </div>
         <div className="mb-3 pt-0">
           <textarea
-            placeholder="Your message"
+            placeholder="Message"
             name="message"
-            className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+            className="border 3px border-black px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:ring w-full"
             required
           />
         </div>
-        <div className="mb-3 pt-0">
+        <div className="mb-3 pt-0 flex justify-center">
           <button
-            className="bg-black text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+            className=" bg-black text-white active:bg-blue-600 font-bold uppercase text-sm px-2 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
             type="submit"
           >
-            Send a message
+            Envoyer le message
           </button>
         </div>
       </form>
+      <div>
+        <h2 className="text-bargreen text-center text-4xl  text-stroke">
+          #BREWDOG FAMILY
+        </h2>
+      </div>
     </div>
   );
 };
