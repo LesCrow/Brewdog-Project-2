3c1f5a7626f55a744eea64c7e90be8f86d1d7d74
LesCrow <john.baruteau@gmail.com>
2022-11-03 22:32:35 +0100

    to be sure commit

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function SearchBar() {
   return (
     <div>
-      <input placeholder="Search" className="w-16 mr-4 h-4 rounded-md" />
+      <input placeholder="Search" className="w-16 mr-4 h-4 rounded-md ml-2 " />
     </div>
   );
 }
