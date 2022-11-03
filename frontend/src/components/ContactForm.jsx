00e8b16ef7ba3a88b76db314751457be9ae0d6bf
sebastienmoulin <sebastien.m.web@gmail.com>
2022-11-03 12:12:03 +0100

    img

--- frontend/src/components/ContactForm.jsx
+++ frontend/src/components/ContactForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { motion } from "framer-motion";
 
 const FORM_ENDPOINT = "";
 
@@ -32,52 +33,76 @@ const ContactForm = () => {
           </h1>
           <img className="p-5" src="src/assets/dogtop.jpeg" alt="banimage" />
         </div>
+        <div className="flex flex-row-reverse">
+          <form
+            className="bg-backgreen border 3px border-black	shadow-md rounded px-8 pt-6 pb-8 m-5"
+            action={FORM_ENDPOINT}
+            onSubmit={handleSubmit}
+            method="POST"
+            target="_blank"
+          >
+            <p className="flex justify-center">LAISSEZ NOUS UN MESSAGE</p>
 
-        <form
-          className="bg-backgreen border 3px border-black	shadow-md rounded px-8 pt-6 pb-8 m-5"
-          action={FORM_ENDPOINT}
-          onSubmit={handleSubmit}
-          method="POST"
-          target="_blank"
-        >
-          <p className="flex justify-center">LAISSEZ NOUS UN MESSAGE</p>
-
-          <div className="mb-3 pt-0 my-8">
-            <input
-              type="text"
-              placeholder="Nom"
-              name="name"
-              className="border 3px border-black	px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm  shadow outline-none focus:outline-none focus:ring w-full"
-              required
+            <div className="mb-3 pt-0 my-8">
+              <input
+                type="text"
+                placeholder="Nom"
+                name="name"
+                className="border 3px border-black	px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm  shadow outline-none focus:outline-none focus:ring w-full"
+                required
+              />
+            </div>
+            <div className="mb-3 pt-0">
+              <input
+                type="email"
+                placeholder="Email"
+                name="email"
+                className="border 3px border-black px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm  shadow outline-none focus:outline-none focus:ring w-full"
+                required
+              />
+            </div>
+            <div className="mb-3 pt-0">
+              <textarea
+                placeholder="Message"
+                name="message"
+                className="border 3px border-black px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:ring w-full"
+                required
+              />
+            </div>
+            <div className="mb-3 pt-0 flex justify-center">
+              <button
+                className=" bg-black text-white active:bg-blue-600 font-bold uppercase text-sm px-2 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                type="submit"
+              >
+                Envoyer
+              </button>
+            </div>
+          </form>
+          <motion.div
+            initial={{ x: 1000 }}
+            animate={{ x: 100 }}
+            transition={{ delay: 1 }}
+            className="text-start flex justify-start"
+          >
+            <img
+              className="text-xs objet-cover h-48 w-auto"
+              src="src/assets/space.jpeg"
+              alt="spaceimage"
             />
-          </div>
-          <div className="mb-3 pt-0">
-            <input
-              type="email"
-              placeholder="Email"
-              name="email"
-              className="border 3px border-black px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm  shadow outline-none focus:outline-none focus:ring w-full"
-              required
+          </motion.div>
+          <motion.div
+            initial={{ x: -1000 }}
+            animate={{ x: 300 }}
+            transition={{ delay: 2 }}
+            className="text-start flex justify-start"
+          >
+            <img
+              className="text-xs object-cover h-48 w-auto mt-20"
+              src="src/assets/concert.jpeg"
+              alt="concertimage"
             />
-          </div>
-          <div className="mb-3 pt-0">
-            <textarea
-              placeholder="Message"
-              name="message"
-              className="border 3px border-black px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:ring w-full"
-              required
-            />
-          </div>
-          <div className="mb-3 pt-0 flex justify-center">
-            <button
-              className=" bg-black text-white active:bg-blue-600 font-bold uppercase text-sm px-2 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-              type="submit"
-            >
-              Envoyer
-            </button>
-          </div>
-        </form>
-
+          </motion.div>
+        </div>
         <div>
           <h2 className="text-bargreen text-center text-4xl  text-stroke ">
             #BREWDOG FAMILY
