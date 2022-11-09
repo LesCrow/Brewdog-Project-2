081dc48ed6eda8a85997a213341717779827ff54
Damsbot64 <damien64durand@gmail.com>
2022-11-10 00:34:14 +0100

    play with framer motion

--- frontend/src/components/BeersCards.jsx
+++ frontend/src/components/BeersCards.jsx
@@ -1,7 +1,21 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
+import { motion, AnimatePresence } from "framer-motion";
 
-import { motion } from "framer-motion";
+const dropIn = {
+  hidden: { y: "-100vh" },
+  visible: {
+    y: "0",
+    opacity: "1",
+    transition: {
+      duration: 0.9,
+      type: "spring",
+      damping: 15,
+      stiffness: 300,
+    },
+  },
+  exit: { y: "100vh" },
+};
 
 function BeersCards({ beer, isActive }) {
   const [isDemo, setIsDemo] = useState(false);
@@ -23,16 +37,14 @@ function BeersCards({ beer, isActive }) {
   }
 
   return (
-    <div className="flex flex-row  h-96">
-      <motion.li
-        animate={{ scale: isOpacity ? 1.2 : 1 }}
-        initial={{ scale: 1 }}
-        className="relative flex flex-col justify-center items-center"
+    <div className="flex flex-row">
+      <li
+        className="relative flex flex-col justify-end items-center"
         onMouseEnter={HandleOverOpacity}
         onMouseLeave={HandleOverOpacity}
       >
         {isOpacity && (
-          <div className="absolute">
+          <div className="absolute m-36">
             <p className="text-bargreen font-semibold">{beer.tagline}</p>
           </div>
         )}
@@ -55,43 +67,83 @@ function BeersCards({ beer, isActive }) {
           <h2 className="hover:underline-offset-auto  ">{beer.name}</h2>
           <p className="text-bargreen font-semibold underline"> {beer.abv}%</p>
           <p className="text-backpink font-semibold">{beer.target_fg} JA$</p>
-          {isActive ? (
+          {isActive && (
             <p className="">
               {beer.food_pairing.map((e) => (
                 <li className="">{e}.</li>
               ))}
             </p>
-          ) : (
-            ""
           )}
         </div>
         <div className="flex flex-col justify-center items-center">
-          <button
+          <motion.button
+            whileHover={{ scale: 1.2 }}
             type="button"
             className="text-sm bg-bargreen rounded-md flex justify-center items-center w-20 mb-2 h-6"
           >
             Panier
-          </button>
+          </motion.button>
 
-          <button
+          <motion.button
+            whileHover={{ scale: 1.2 }}
             type="button"
             className="text-sm bg-backpink rounded-md flex justify-center items-center w-28  h-6"
             onClick={HandleOverDescription}
           >
             Description
-          </button>
-          {showDescription ? (
-            <div className="absolute bg-slate-600 h-96 w-full">
-              <h1>{beer.description}</h1>
-              <p onClick={closePopUp} className="text-lg font-extrabold">
-                X
-              </p>
-            </div>
-          ) : (
-            ""
-          )}
+          </motion.button>
+          <AnimatePresence initial={false} onExitComplete={() => null}>
+            {" "}
+            {showDescription && (
+              <motion.div
+                variants={dropIn}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                onClick={() => setShowDescription(!showDescription)}
+                className="fixed  inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center z-10"
+              >
+                <div className="w-[600px] flex flex-col bg-white bg-opacity-70 rounded-lg">
+                  <motion.button
+                    whileHover={{ scale: 1.2 }}
+                    className="text-white text-xl"
+                    type="button"
+                    onClick={closePopUp}
+                  >
+                    X
+                  </motion.button>
+                  <div className="p-2 flex flex-col items-center">
+                    <img
+                      className="h-52 w-16 "
+                      src={beer.image_url}
+                      alt="Une biere"
+                    />
+                    <h2 className="">{beer.name}</h2>
+
+                    <h1>{beer.description}</h1>
+                  </div>
+                  <div className="flex flex-row justify-around">
+                    <p className="text-bargreen font-semibold underline ">
+                      {" "}
+                      {beer.abv}%
+                    </p>
+                    <p className="text-backpink font-semibold ">
+                      {beer.target_fg} JA$
+                    </p>
+                    <motion.button
+                      whileHover={{ scale: 1.2 }}
+                      type="button"
+                      className="text-sm bg-bargreen rounded-md   w-20 mb-2 h-6 "
+                    >
+                      Panier
+                    </motion.button>
+                  </div>
+                </div>
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
-      </motion.li>
+      </li>
     </div>
   );
 }
