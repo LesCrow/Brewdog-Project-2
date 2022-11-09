31cd99d9c3b0df71e60daead2cf503223914bff1
LesCrow <john.baruteau@gmail.com>
2022-11-09 10:11:23 +0100

    layout added, pop up intagrated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,20 @@
-// import { ToastContainer } from "react-toastify";
-import React from "react";
+import { ToastContainer } from "react-toastify";
+import { React, useState } from "react";
 
-import PageBeers from "./components/PageBeers";
-
-// import Beers from "@components/Beers";
 // import ContactForm from "@components/ContactForm";
-// import Brewery from "./components/Brewery";
-//
+import Brewery from "./components/Brewery";
 
-// import Footer from "./components/Footer";
-// import Age from "./components/Age";
-// import "react-toastify/dist/ReactToastify.css";
+import Age from "./components/Age";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  // const [isVerified, setIsVerified] = useState(false);
+  const [isVerified, setIsVerified] = useState(false);
 
   return (
     <div>
-      <PageBeers />
+      {!isVerified && <Age setIsVerified={setIsVerified} />}
+      <ToastContainer />
+      <Brewery />
       {/* <Header
         handleDisplayBurger={handleDisplayBurger}
         isBurgerMenuOpen={isBurgerMenuOpen}
@@ -25,16 +22,14 @@ function App() {
 
      
       
-      {!isVerified && <Age setIsVerified={setIsVerified} />}
-      <Brewery />
+      
+      
 
-      <ToastContainer />
-      <Beers />
+    
       <div className="w-screen bg-black min-h-screen flex relative items-center align-middle justify-center">
         <ContactForm />{" "}
       </div>
-      <div className="w-screen flex bg-black">
-        <Footer />
+
       </div> */}
     </div>
   );
