ef3dc4d5a3b8c1451c65fcd7c48795aca1890903
sebastienmoulin <sebastien.m.web@gmail.com>
2022-11-10 16:36:49 +0100

    Scroll Popup in progress

--- frontend/src/components/ContactForm.jsx
+++ frontend/src/components/ContactForm.jsx
@@ -14,8 +14,8 @@ function ContactForm() {
   if (submitted) {
     return (
       <>
-        <div className="text-2xl">Merci!</div>
-        <div className="text-md">Nous vous contacterons bientôt.</div>
+        <div className="text-2xl">Thanks!</div>
+        <div className="text-md">We will contact you soon.</div>
       </>
     );
   }
@@ -34,24 +34,24 @@ function ContactForm() {
         </div>
         <div className="flex justify-center sm:justify-between w-full flex-row-reverse">
           <form
-            className="bg-backgreen border 3px border-black	shadow-md rounded px-52 pt-40 pb-8 m-10"
+            className="bg-backgreen border 3px border-black	shadow-md rounded w-1/3 px-14 pt-20 pb-8 m-10"
             action={FORM_ENDPOINT}
             onSubmit={handleSubmit}
             method="POST"
             target="_blank"
           >
-            <p className="flex justify-center">LAISSEZ NOUS UN MESSAGE</p>
+            <p className="flex justify-center">LEAVE US A MESSAGE</p>
 
-            <div className="mb-3 pt-0 my-8">
+            <div className="mb-3 pt-5 my-8">
               <input
                 type="text"
-                placeholder="Nom"
-                name="name"
+                placeholder="Last name"
+                name="Last name"
                 className="border 3px border-black	px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white rounded text-sm  shadow outline-none focus:outline-none focus:ring w-full"
                 required
               />
             </div>
-            <div className="mb-3 pt-0 ">
+            <div className="mb-3 pt-5 ">
               <input
                 type="email"
                 placeholder="Email"
@@ -60,7 +60,7 @@ function ContactForm() {
                 required
               />
             </div>
-            <div className="mb-3 pt-0 ">
+            <div className="mb-3 pt-5 ">
               <input
                 type="Object"
                 placeholder="Object"
@@ -70,7 +70,7 @@ function ContactForm() {
               />
             </div>
 
-            <div className="mb-3 pt-0">
+            <div className="mb-3 pt-5">
               <textarea
                 placeholder="Message"
                 name="message"
@@ -78,18 +78,19 @@ function ContactForm() {
                 required
               />
             </div>
-            <div className="mb-3 pt-0 flex justify-center">
-              <button
+            <div className="mb-3 pt-10 flex justify-end">
+              <motion.button
+                whileHover={{ scale: 1.2 }}
                 className=" bg-black text-white active:bg-blue-600 font-bold uppercase text-sm px-2 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                 type="submit"
               >
-                Envoyer
-              </button>
+                Send
+              </motion.button>
             </div>
           </form>
           <div className="relative hidden sm:flex md:flex lg:flex  flex-col">
             <motion.div
-              initial={{ x: 1000 }}
+              initial={{ x: -1000 }}
               animate={{ x: 100 }}
               transition={{ delay: 1 }}
               className="text-start relative flex my-10 justify-start w-96 "
