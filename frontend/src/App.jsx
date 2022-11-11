acb496e732b2ac91b91152a62d030484af731e6c
LesCrow <john.baruteau@gmail.com>
2022-11-11 16:41:31 +0100

    url routes for home and shop

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,27 @@
 import { ToastContainer } from "react-toastify";
 import React, { useState } from "react";
-import Home from "./components/Home";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
 import Age from "./components/Age";
-import Beers from "./components/Beers";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const [isVerified, setIsVerified] = useState(false);
 
   return (
-    <div>
+    <BrowserRouter>
       {!isVerified && <Age setIsVerified={setIsVerified} />}
       <ToastContainer />
-      <Home />
-      <Beers />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        {/* Si l'url ne corresond à rien de déclaré */}
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
