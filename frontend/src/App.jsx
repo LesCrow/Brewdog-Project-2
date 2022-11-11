8ffb2a10919e9c01dd529525986cd2b93bab912e
LesCrow <john.baruteau@gmail.com>
2022-11-11 18:45:16 +0100

    end of the day commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import { ToastContainer } from "react-toastify";
 import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "react-toastify/dist/ReactToastify.css";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import Brewery from "./pages/Brewery";
 import Age from "./components/pop-up/Age";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-
-import "react-toastify/dist/ReactToastify.css";
+import ContactForm from "./components/contact/ContactForm";
 
 function App() {
   const [isVerified, setIsVerified] = useState(false);
@@ -20,8 +20,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/brewery" element={<Brewery />} />
+        <Route path="/contact" element={<ContactForm />} />
         {/* Si l'url ne corresond à rien de déclaré */}
-        <Route path="/*" element={<Home />} />
+        <Route path="*" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -4,8 +4,8 @@ import useWindowSize from "../../hooks/useWindowDimension";
 import useOnClickOutside from "../../hooks/useOnClickOutside";
 import Header from "./Header";
 import Footer from "./Footer";
-import MenuListMobile from "../header/MenuListMobile";
-import MenuListDesktop from "../header/MenuListDesktop";
+import MenuListMobile from "../header/MenuMobile";
+import MenuListDesktop from "../header/MenuDesktop";
 
 function Layout({ children }) {
   // Hook to get window size
