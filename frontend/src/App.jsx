077514b52c61ed87360d0d8d47fd53be8665919f
LesCrow <john.baruteau@gmail.com>
2022-11-16 11:05:37 +0100

    conflicts resolved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import Home from "./components/Home";
 import ContactForm from "./components/ContactForm";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 function App() {
   return (
     <div>
-      <Home />
-
       <ContactForm />
     </div>
   );
