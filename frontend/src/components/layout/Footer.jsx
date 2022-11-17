15e2d19f16090920b78dc8b59ed0a27cd965e543
LesCrow <john.baruteau@gmail.com>
2022-11-17 14:49:11 +0100

    footer links done

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -1,16 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { navLinks } from "../../utils/constants";
 import LegalNotices from "../footer/LegalNotices";
 import Logo from "../global/Logo";
-import NavLinks from "../global/NavLinks";
+// import NavLinks from "../global/NavLinks";
 import NetworkLinks from "../footer/NetworkLinks";
 import NewsletterInput from "../footer/NewsletterInput";
 
 function Footer() {
   return (
     <div className="flex flex-col justify-center items-center p-5 bg-navBlue w-full h-354 relative z-50">
-      <Logo />
+      <Link to="/">
+        <Logo />
+      </Link>
       <NewsletterInput />
-      <NavLinks />
+      <div className="flex w-1/2 justify-between font-anton text-white text-2xl text-stroke mt-8">
+        {navLinks.map((link) => (
+          <Link to={link.to}>{link.page}</Link>
+        ))}
+      </div>
       <NetworkLinks />
       <LegalNotices />
     </div>
