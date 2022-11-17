fb126b0c87ee72f921d12fb74d4cd19e44584d73
LesCrow <john.baruteau@gmail.com>
2022-11-17 12:30:37 +0100

    carousel settings modified

--- frontend/src/components/home/Carousel.jsx
+++ frontend/src/components/home/Carousel.jsx
@@ -5,7 +5,14 @@ export default function MyCarousel() {
   return (
     <div className="flex justify-center items-center">
       <div className="flex  md:w-1/2 md:flex lg:felx lg:1/3 ">
-        <Carousel infiniteLoop showThumbs={false} autoPlay>
+        <Carousel
+          infiniteLoop
+          showThumbs={false}
+          showArrows={false}
+          showStatus={false}
+          autoPlay
+          interval={2000}
+        >
           <div>
             <img alt="" src="src/assets/gluten-free_punk_-_1050x1050_1_1.png" />
           </div>
