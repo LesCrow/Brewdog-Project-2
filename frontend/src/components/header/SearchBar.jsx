21a8ad944d196587e0f8d706242c03d6254dcca5
LesCrow <john.baruteau@gmail.com>
2022-11-20 15:34:39 +0100

    search bar linked to shop

--- frontend/src/components/header/SearchBar.jsx
+++ frontend/src/components/header/SearchBar.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function SearchBar() {
+  const [filtered, setFiltered] = useState("");
+
   return (
     <div>
-      <input
-        placeholder="Search a beer"
-        className="h-8 w-16 md:w-[80%] mr-4  rounded-md ml-2 "
-      />
+      <Link to="/shop">
+        <input
+          type="text"
+          placeholder="Search a beer"
+          className="h-8 w-16 md:w-[80%] mr-4  rounded-md ml-2 "
+          onChange={(e) => {
+            setFiltered(e.target.value);
+          }}
+        />
+        {console.error(filtered)}
+      </Link>
     </div>
   );
 }
